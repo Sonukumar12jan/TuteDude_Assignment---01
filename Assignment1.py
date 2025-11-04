d94e37fb31813ca518f8f5451b124453725a0151
sonu kumar <163671481+Sonukumar12jan@users.noreply.github.com>
2025-11-04 22:12:17 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,21 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("\nAddition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+#**************************************************************************
+print("\n")
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print(f"\nHello, {first_name} {last_name}! Welcome to the Python program.")
+
